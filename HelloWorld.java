54fd37a726ea039b8666ea7fbc4947ae8d199bda
sanketachordiya <sanketachordiya@gmail.com>
2020-04-27 14:56:42 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("*****Welcome to Jenkins Training******");
+		System.out.println("*****Welcome to Jenkins Training Testing Poll SCM ******");
 	}
 }
